a38dc5d34edba8a59c8bc8a705470020d3c97beb
Nikoloman <hectorvel52@gmail.com>
2022-06-12 22:52:36 -0500

    Version 0.0.1.1 I tried to code a floating action button, BUT I COULDN'T I DON'T KNOW WHY!!1!1!!1 I'll try to solve it later or just put a normal button at the top of the activity

--- app/src/main/java/com/example/upnotes/MainActivity.java
+++ app/src/main/java/com/example/upnotes/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.upnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        FloatingActionButton fab = findViewById(R.id.NewNote);
+    }
+
+    public void New_Note(View view){
+        Toast.makeText(this, "The floating action button works", Toast.LENGTH_SHORT).show();
     }
 }
